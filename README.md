f102cdf1a597de1296d7a769591acdac8f4c02b8
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-12-11 13:56:41 +0100

    PR fixes

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ Bitrise CLI Example Plugin
 Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise).
 
 First install the plugin:
-`bitrise plugin-install \
-  --source https://github.com/bitrise-tools/bitrise-plugin-hello-world/releases/download/0.1.0/bitrise-plugin-hello-world \
-  --name hello`
+`bitrise plugin-install --source https://github.com/bitrise-tools/bitrise-plugin-hello-world/releases/download/0.1.0/bitrise-plugin-hello-world --name hello`
 
 After that, you can use it:
 `bitrise :hello`
+
+## Guidelines for Plugin Development
+  * Plugin outputs and errors should printed to the STDERR
+  * bitrise informations are passed to the Plugin as JSON string on STDIN
+  * Plugin can pass informations to bitrise through STDOUT as JSON string
--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 )
 
+// Print errors to os.Stderr
 func printFatal(format string, a ...interface{}) {
 	msg := fmt.Sprintf(format, a...)
 	fmt.Fprintf(os.Stderr, "\x1b[31;1m%s\x1b[0m\n", msg)
 	os.Exit(1)
 }
 
-func printHello(name string) {
-	msg := fmt.Sprintf("Hello %s!", name)
+// Print outputs to os.Stderr
+func printOutput(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
 	fmt.Fprintf(os.Stderr, "\x1b[32;1m%s\x1b[0m\n", msg)
 }
 
-func printBitriseInfo() {
+// Send infos to bitrise througth os.Stdout as JSON string
+func sendBitriseInfo() {
 	msg := map[string]string{
 		"bitrise-info": "Bitrise CLI plugin sample",
 	}
@@ -30,25 +34,25 @@ func printBitriseInfo() {
 	fmt.Fprintf(os.Stdout, "%s\n", msgJSON)
 }
 
-func parseCli(args []string) (string, error) {
-	if len(args) > 2 {
-		return "", fmt.Errorf("Only one argument allowed, provided (%d)", len(os.Args)-1)
+// Bitrise send informations to Plugin on os.Stdin as JSON string
+func parseBitriseInfo() (string, error) {
+	args := os.Args
+	if len(args) < 2 {
+		return "", errors.New("Invalid plugin usage, no bitrise infos passed to the plugin")
 	}
 
-	name := "Bitriser"
-	if len(args) == 2 {
-		name = args[1]
-	}
-	return name, nil
+	infos := args[1]
+	return infos, nil
 }
 
 func main() {
 	// Parse cli
-	name, err := parseCli(os.Args)
+	infos, err := parseBitriseInfo()
 	if err != nil {
 		printFatal("Failed to parse cli, err: %s", err)
 	}
 
-	printHello(name)
-	printBitriseInfo()
+	printOutput("Hello World!")
+	printOutput("Bitrise infos: %s", infos)
+	sendBitriseInfo()
 }
