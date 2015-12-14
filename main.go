026dbef6eecb1c005684c2b0c21dd34df5a07796
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-12-14 11:50:29 +0100

    requirements

--- main.go
+++ main.go
@@ -7,52 +7,131 @@ import (
 	"os"
 )
 
+//-----------------------------------
+// Communication
+//-----------------------------------
+
+// Plugin output (logs, errors, ...) should send to os.Stderr
+// Messages to bitrise (plugin requirements, plugin version, ...) should send to os.Stdout as JSON string
+
 // Print errors to os.Stderr
-func printFatal(format string, a ...interface{}) {
+func sendFatal(format string, a ...interface{}) {
 	msg := fmt.Sprintf(format, a...)
 	fmt.Fprintf(os.Stderr, "\x1b[31;1m%s\x1b[0m\n", msg)
 	os.Exit(1)
 }
 
 // Print outputs to os.Stderr
-func printOutput(format string, a ...interface{}) {
+func sendMessageToOutput(format string, a ...interface{}) {
 	msg := fmt.Sprintf(format, a...)
 	fmt.Fprintf(os.Stderr, "\x1b[32;1m%s\x1b[0m\n", msg)
 }
 
-// Send infos to bitrise througth os.Stdout as JSON string
-func sendBitriseInfo() {
+// Send message to bitrise througth os.Stdout as JSON string
+func sendMessageToBitrise(format string, a ...interface{}) {
+	msgStr := fmt.Sprintf(format, a...)
 	msg := map[string]string{
-		"bitrise-info": "Bitrise CLI plugin sample",
+		"bitrise-info": msgStr,
 	}
 
 	msgJSON, err := json.Marshal(msg)
 	if err != nil {
-		printFatal("%s", err)
+		sendFatal("%s", err)
 	}
 
 	fmt.Fprintf(os.Stdout, "%s\n", msgJSON)
 }
 
-// Bitrise send informations to Plugin on os.Stdin as JSON string
-func parseBitriseInfo() (string, error) {
-	args := os.Args
-	if len(args) < 2 {
-		return "", errors.New("Invalid plugin usage, no bitrise infos passed to the plugin")
-	}
+//-----------------------------------
+// CLI
+//-----------------------------------
 
-	infos := args[1]
-	return infos, nil
+// Command model
+type Command struct {
+	Name string
+	Func func() error
 }
 
-func main() {
-	// Parse cli
-	infos, err := parseBitriseInfo()
-	if err != nil {
-		printFatal("Failed to parse cli, err: %s", err)
+// Available commands
+var commands = []Command{
+	Command{
+		Name: "requirements",
+		Func: requirements,
+	},
+}
+
+// Requirements command, all plugin should implement this.
+// After installing a plugin, bitrise ask a plugin for plugin requirements.
+func requirements() error {
+	type Requirement struct {
+		ToolName   string
+		MinVersion string
+		MaxVersion string
 	}
 
-	printOutput("Hello World!")
-	printOutput("Bitrise infos: %s", infos)
-	sendBitriseInfo()
+	var requirements = []Requirement{
+		Requirement{
+			ToolName:   "bitrise",
+			MinVersion: "1.3.0",
+			MaxVersion: "",
+		},
+		Requirement{
+			ToolName:   "stepman",
+			MinVersion: "1.3.0",
+			MaxVersion: "",
+		},
+		Requirement{
+			ToolName:   "envman",
+			MinVersion: "1.3.0",
+			MaxVersion: "",
+		},
+	}
+
+	bytes, err := json.Marshal(requirements)
+	if err != nil {
+		return err
+	}
+
+	sendMessageToBitrise(string(bytes))
+
+	return nil
+}
+
+// Bitrise send messages to Plugin on os.Stdin as JSON string.
+// Message can be request for command (ex: requirements, doSomething, ...),
+// or informations from bitrise (ex: bitrise version, bitrise workflow path, ...)
+func parseCli() (string, error) {
+	args := os.Args
+	if len(args) < 2 {
+		return "", errors.New("Invalid plugin usage, no bitrise infos, even command passed to the plugin")
+	}
+
+	msg := os.Args[1]
+	return msg, nil
+}
+
+//-----------------------------------
+// Main
+//-----------------------------------
+
+func main() {
+	message, err := parseCli()
+	if err != nil {
+		sendFatal("Failed to parse cli, err: %s", err)
+	}
+
+	messageIsCommand := false
+	for _, command := range commands {
+		if command.Name == message {
+			if err := command.Func(); err != nil {
+				sendFatal("Failed to perform command (%s), err: %s", command.Name, err)
+			}
+			messageIsCommand = true
+		}
+	}
+
+	if !messageIsCommand {
+		sendMessageToOutput("Bitrise message: %s", message)
+		sendMessageToOutput("Hello World!")
+	}
 }
